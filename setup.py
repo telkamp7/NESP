1d631e388110b2fa3710a5364b5d67ad616d1115
root <root@LAPTOP-6TC92KKV.localdomain>
2022-10-20 15:48:42 +0200

    Initialized project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The goal of this repository is to predict the thermostability of enzyme variants. Understanding and accurately predicting protein stability is a fundamental problem in biotechnology.',
+    author='Kasper Schou Telkamp',
+    license='MIT',
+)
